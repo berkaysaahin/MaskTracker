a97948d40fae07df0f5df32fe6de3a91f2feff72
berkaysaahin <berkaynihas@gmail.com>
2024-02-09 21:35:28 +0300

    first commit

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Citizen citizen)
+        {
+            
+            if (_applicantService.CheckCitizen(citizen) ) {
+                Console.WriteLine(citizen.FirstName + " named user received a mask.");
+            }
+            else 
+            {
+                Console.WriteLine(citizen.FirstName + " named user didn't receive a mask.");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Citizen citizen1 = new Citizen();
+        citizen1.NationalIdentity = 12345;
+        citizen1.FirstName = "BERKAY";
+        citizen1.LastName = "ŞAHİN";
+        citizen1.DateOfBirth = 2000;
+
+        PttManager pttManager = new PttManager(new CitizenManager());
+        pttManager.GiveMask(citizen1 );
+        Console.ReadLine();
+    }
+}
